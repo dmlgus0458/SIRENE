735bf1db7dfe63d48c5c64ac9ec8feab21a60c66
KimUiHyun <dmlgus0458@gmail.com>
2024-04-04 11:10:40 +0900

    Add) Footer 추가

--- src/features/Main/Main.js
+++ src/features/Main/Main.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from '../../layout/Header';
+import Footer from '../../layout/Footer';
 import MainSwiper from './components/MainSwiper';
 import ItemList from './components/ItemList';
+
 const Main = () => {
     return (
         <>
             <Header />
             <MainSwiper />
             <ItemList />
+            <Footer />
         </>
     );
 };
