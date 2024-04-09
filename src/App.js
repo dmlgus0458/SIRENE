9e3a6c9ce9338880a18bfb26c79203458f2d1ade
KimUiHyun <dmlgus0458@gmail.com>
2024-04-09 09:48:38 +0900

    Feat) 회원가입 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './features/Main/Main';
+import Registration from './features/Register/Registration';
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Main />}></Route>
+                    <Route path="/register" element={<Registration />}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
