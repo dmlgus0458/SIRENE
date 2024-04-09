66a8bb7879882d03c123ed94818864a88832bf74
KimUiHyun <dmlgus0458@gmail.com>
2024-04-09 10:04:02 +0900

    Feat) 회원가입 폼을 포함하는 회원가입 페이지 레이아웃 생성

---
+++ src/features/Register/Registration.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RegisterForm from './RegisterForm';
+import Header from '../../layout/Header';
+import Footer from '../../layout/Footer';
+
+const Registration = () => {
+    return (
+        <>
+            <Header />
+            <RegisterForm />
+            <Footer />
+        </>
+    );
+};
+
+export default Registration;
